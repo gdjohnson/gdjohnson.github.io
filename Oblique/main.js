31856722a432d4da59ab018dbc23ab78a836b48b
Suspended Reason <suspendedreason@Suspendeds-MacBook-Pro-2.local>
2026-01-08 21:58:14 -0500

    Rm rest

--- Oblique/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    phrases = ["Who is affected?", "Who thinks otherwise?"];
-    idx = Math.floor(phrases.length * Math.random());
-    const phrase = phrases[idx]
-    if (document.getElementById('main')){
-        document.getElementById('main').innerHTML = phrase;
-    }
-  });
